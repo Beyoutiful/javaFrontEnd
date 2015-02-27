79d857970a6525d32d300430ee84b8d793635bb2
Brandon Foss <Bfoss@mavs.coloradomesa.edu>
2015-02-27 14:14:27 -0700

    trying to change repo

---
+++ javaFrontEnd/src/javafrontend/Item.java
@@ -0,0 +1,92 @@
+package javafrontend;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBDecoder;
+import com.mongodb.DBEncoder;
+import com.mongodb.DBObject;
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ReadPreference;
+import com.mongodb.WriteConcern;
+import com.mongodb.WriteResult;
+import javafx.scene.control.ChoiceBox;
+/**
+ * @author sean
+ * Implements Items collection from database
+ * See end of Item.java for original methods from DocumentController I've
+ * replaced --Sean, 26 Feb 2015
+ */
+
+public class Item 
+{
+    private DBCollection table;
+    private BasicDBObject searchQuery;
+    
+    public Item()
+    {
+        this.table = BEYOU_DB.getCollection("items");
+        this.searchQuery = new BasicDBObject();
+        
+    }
+    
+    public void getItems()
+    {
+        DBCursor cursor = table.find(searchQuery);
+        
+        while (cursor.hasNext())
+        {
+            DBObject item = cursor.next();
+            
+            System.out.println(item.get("_id"));
+            choiceBox.getItems().add(item.get("name"));
+        }
+    }
+    
+    public void populateItem()
+    {
+        DBCursor cursor = table.find(searchQuery);
+        
+        while (cursor.hasNext()) 
+        {
+            DBObject item = cursor.next();
+            
+            itemID.setText((String) item.get("_id"));
+            itemName.setText((String) item.get("name"));
+            itemPrice.setText((String) item.get("price"));
+            itemDescription.setText((String) item.get("description"));
+    }
+}
+
+    //***Below are the methods I've attempted to replace with the class Item -SEAN***
+    /*public void getItems() 
+    {
+        DBCollection table = BEYOU_DB.getCollection("items");
+        BasicDBObject searchQuery = new BasicDBObject();
+        DBCursor cursor = table.find(searchQuery);
+
+        while (cursor.hasNext()) 
+        {
+            DBObject item = cursor.next();
+            System.out.println(item.get("_id"));
+            choiceBox.getItems().add(item.get("name"));
+        }
+    }
+
+    public void populateItem() 
+    {
+        DBCollection table = BEYOU_DB.getCollection("items");
+        BasicDBObject searchQuery = new BasicDBObject();
+        DBCursor cursor = table.find(searchQuery);
+
+        while (cursor.hasNext()) {
+            DBObject item = cursor.next();
+            itemID.setText((String) item.get("_id"));
+            itemName.setText((String) item.get("name"));
+            itemPrice.setText((String) item.get("price"));
+            itemDescription.setText((String) item.get("description"));
+        }
+    }*/
