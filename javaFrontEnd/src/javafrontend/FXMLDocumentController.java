038ca173d873706bae462726d3b3cf1eba2617f6
Brandon Foss <Bfoss@mavs.coloradomesa.edu>
2015-03-03 08:17:25 -0700

    working with the item Class, still have null pointer exception that i cant track down.

--- javaFrontEnd/src/javafrontend/FXMLDocumentController.java
+++ javaFrontEnd/src/javafrontend/FXMLDocumentController.java
@@ -9,15 +9,8 @@ import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
-import com.mongodb.DBDecoder;
-import com.mongodb.DBEncoder;
 import com.mongodb.DBObject;
-import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
-import com.mongodb.MongoCredential;
-import com.mongodb.ReadPreference;
-import com.mongodb.WriteConcern;
-import com.mongodb.WriteResult;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -25,15 +18,10 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.UnknownHostException;
-import java.util.Date;
-import java.util.Iterator;
-import java.util.Map;
 import java.util.ResourceBundle;
-import java.util.Set;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import static javafx.scene.Cursor.cursor;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
@@ -44,15 +32,15 @@ import javafx.scene.control.Tab;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import org.bson.BSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+
 /**
  *
  * @author FossRobotics
  */
 public class FXMLDocumentController implements Initializable {
-    
+
     @FXML
     private Label passLabel;
     @FXML
@@ -66,19 +54,19 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private Label clientEmail;
     @FXML
-    private Button submit;
+    private Label loginLabel;
     @FXML
-    private Button button;
+    private Button submit;
     @FXML
     private Button searchButton;
     @FXML
-    private Button newClientButton;
+    private Button saveButton;
     @FXML
-    private Tab admin;
+    private Tab client;
     @FXML
     private Tab login;
     @FXML
-    private Tab schedule;
+    private Tab items;
     @FXML
     private DatePicker datePicker;
     @FXML
@@ -111,182 +99,196 @@ public class FXMLDocumentController implements Initializable {
     private TextField itemID;
     @FXML
     private TextField itemName;
-     @FXML
+    @FXML
     private TextField itemPrice;
     @FXML
     private TextArea itemDescription;
-   
+
     private final MongoClient mongoClient;
 
     public FXMLDocumentController() throws UnknownHostException {
-        this.mongoClient = new MongoClient("ds035750.mongolab.com",35750);
-       
+        this.mongoClient = new MongoClient("ds035750.mongolab.com", 35750);
+
     }
 
-    
     @FXML
-    private void handleButtonAction(ActionEvent event) throws IOException, MalformedURLException, ParseException  {
-     login();
+    private void handleButtonAction(ActionEvent event) throws IOException, MalformedURLException, ParseException {
+       // login();
     }
-     @FXML
-    private void handleSearchButtonAction(ActionEvent event)  {
-    
+
+    @FXML
+    private void handleSearchButtonAction(ActionEvent event) {
+
         getName(clientNameField.getText());
     }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-        
+
     }
-    public void database(){
-       
+
+    public void database() {
+
         DB db = mongoClient.getDB("heroku_app33977271");
-        boolean auth = db.authenticate("beyoutiful","P00k!ooFff".toCharArray());
+        boolean auth = db.authenticate("beyoutiful", "P00k!ooFff".toCharArray());
         //save example
-             System.out.println("auth: "+ auth);
+        System.out.println("auth: " + auth);
         DBCollection table = db.getCollection("services");
         BasicDBObject document = new BasicDBObject();
-        document.put("name","Brandon");
-        document.put("price",65);
+        document.put("name", "Brandon");
+        document.put("price", 65);
         document.put("description", "new toes");
-        document.put("__v",0);
+        document.put("__v", 0);
         table.insert(document);
 
-      
+    }
 
-    
-    } 
-    public void getServices(){
-   
+    public void getServices() {
+
         DB db = mongoClient.getDB("heroku_app33977271");
-        boolean auth = db.authenticate("beyoutiful","P00k!ooFff".toCharArray());
+        boolean auth = db.authenticate("beyoutiful", "P00k!ooFff".toCharArray());
         //save example
-             System.out.println("auth: "+ auth);
+        System.out.println("auth: " + auth);
         DBCollection table = db.getCollection("services");
 
-
-             BasicDBObject searchQuery = new BasicDBObject();
-             searchQuery.put("name", "Manicure");
-
-             DBCursor cursor = table.find(searchQuery);
-
-             while (cursor.hasNext()) {
-                     System.out.println(cursor.next());
-             }
-         }
-    public void getItems(){
-   
-        DB db = mongoClient.getDB("heroku_app33977271");
-        boolean auth = db.authenticate("beyoutiful","P00k!ooFff".toCharArray());
-        //save example
-             System.out.println("auth: "+ auth);
-        DBCollection table = db.getCollection("items");
-
-
-             BasicDBObject searchQuery = new BasicDBObject();
-             
-
-             DBCursor cursor = table.find(searchQuery);
-             
-             while (cursor.hasNext()) {
-                 DBObject item = cursor.next();
-                 System.out.println(item.get("_id"));
-                 choiceBox.getItems().add(item.get("name"));
-             }
-         }
-    public void populateItem(){
-         DB db = mongoClient.getDB("heroku_app33977271");
-         boolean auth = db.authenticate("beyoutiful","P00k!ooFff".toCharArray());
-             DBCollection table = db.getCollection("items");
-             BasicDBObject searchQuery = new BasicDBObject();     
-             DBCursor cursor = table.find(searchQuery);
-             while (cursor.hasNext()) {
-                 DBObject item = cursor.next();
-                itemID.setText((String)item.get("_id"));
-                itemName.setText((String)item.get("name"));
-                itemPrice.setText((String)item.get("price"));
-                itemDescription.setText((String)item.get("description"));
-             }
-    }
-    public void updateCollection(){
-       
-        DB db = mongoClient.getDB("heroku_app33977271");
-        boolean auth = db.authenticate("beyoutiful","P00k!ooFff".toCharArray());
-   
-        System.out.println("auth: "+ auth);
-        DBCollection table = db.getCollection("services");
- 
-	BasicDBObject query = new BasicDBObject();
-	query.put("name", "Manicure-updated");
- 
-	BasicDBObject newDocument = new BasicDBObject();
-	newDocument.put("name", "Manicure");
- 
-	BasicDBObject updateObj = new BasicDBObject();
-	updateObj.put("$set", newDocument);
- 
-	table.update(query, updateObj);
-    }
-    public void getName(String Name){
-        
-        DB db = mongoClient.getDB("heroku_app33977271");
-        boolean auth = db.authenticate("beyoutiful","P00k!ooFff".toCharArray());
-        DBCollection table = db.getCollection("clients");
         BasicDBObject searchQuery = new BasicDBObject();
-        searchQuery.put("name",Name);
+        searchQuery.put("name", "Manicure");
+
         DBCursor cursor = table.find(searchQuery);
-       
-        String ClientName=searchQuery.getString(Name);
-        //System.out.println("Looking for: "+Name);// these are to see the name your searching for is being entered
-         //if (ClientName == null){
-             errorMessage.setText(ClientName);
-            //System.out.println("Cant locate that name.");
-       // }
-	while (cursor.hasNext()) {
-		System.out.println(cursor.next());
-               // System.out.println("name "+ClientName);
-               
+
+        while (cursor.hasNext()) {
+            System.out.println(cursor.next());
         }
     }
-    public void enterClient()
-    {
-      
+
+    public void getItems() {
+
         DB db = mongoClient.getDB("heroku_app33977271");
-        boolean auth = db.authenticate("beyoutiful","P00k!ooFff".toCharArray());
+        boolean auth = db.authenticate("beyoutiful", "P00k!ooFff".toCharArray());
+        //save example
+        System.out.println("auth: " + auth);
+        DBCollection table = db.getCollection("items");
+
+        BasicDBObject searchQuery = new BasicDBObject();
+
+        DBCursor cursor = table.find(searchQuery);
+
+        while (cursor.hasNext()) {
+            DBObject item = cursor.next();
+            System.out.println(item.get("_id"));
+            choiceBox.getItems().add(item.get("name"));
+        }
+    }
+
+    public void populateItem() {
+        DB db = mongoClient.getDB("heroku_app33977271");
+        boolean auth = db.authenticate("beyoutiful", "P00k!ooFff".toCharArray());
+        DBCollection table = db.getCollection("items");
+        BasicDBObject searchQuery = new BasicDBObject();
+        DBCursor cursor = table.find(searchQuery);
+        while (cursor.hasNext()) {
+            DBObject item = cursor.next();
+            itemID.setText((String) item.get("_id"));
+            itemName.setText((String) item.get("name"));
+            itemPrice.setText((String) item.get("price"));
+            itemDescription.setText((String) item.get("description"));
+        }
+    }
+
+    public void updateCollection() {
+
+        DB db = mongoClient.getDB("heroku_app33977271");
+        boolean auth = db.authenticate("beyoutiful", "P00k!ooFff".toCharArray());
+
+        System.out.println("auth: " + auth);
+        DBCollection table = db.getCollection("services");
+
+        BasicDBObject query = new BasicDBObject();
+        query.put("name", "Manicure-updated");
+
+        BasicDBObject newDocument = new BasicDBObject();
+        newDocument.put("name", "Manicure");
+
+        BasicDBObject updateObj = new BasicDBObject();
+        updateObj.put("$set", newDocument);
+
+        table.update(query, updateObj);
+    }
+
+    public void getName(String Name) {
+
+        DB db = mongoClient.getDB("heroku_app33977271");
+        boolean auth = db.authenticate("beyoutiful", "P00k!ooFff".toCharArray());
+        DBCollection table = db.getCollection("clients");
+        BasicDBObject searchQuery = new BasicDBObject();
+        searchQuery.put("name", Name);
+        DBCursor cursor = table.find(searchQuery);
+
+        String ClientName = searchQuery.getString(Name);
+        //System.out.println("Looking for: "+Name);// these are to see the name your searching for is being entered
+        //if (ClientName == null){
+        errorMessage.setText(ClientName);
+        //System.out.println("Cant locate that name.");
+        // }
+        while (cursor.hasNext()) {
+            System.out.println(cursor.next());
+            // System.out.println("name "+ClientName);
+
+        }
+    }
+
+    public void enterClient() {
+
+        DB db = mongoClient.getDB("heroku_app33977271");
+        boolean auth = db.authenticate("beyoutiful", "P00k!ooFff".toCharArray());
 
         DBCollection table = db.getCollection("clients");
         BasicDBObject document = new BasicDBObject();
-        document.put("name",clientNameField.getText());
-        document.put("phone",clientNumberField.getText());
+        document.put("name", clientNameField.getText());
+        document.put("phone", clientNumberField.getText());
         document.put("email", clientEmailField.getText());
         document.put("address", clientAddressField.getText());
-        document.put("__v",0);
+        document.put("__v", 0);
         table.insert(document);
         clientNameField.setText("");
         clientNumberField.setText("");
         clientEmailField.setText("");
         clientAddressField.setText("");
     }
-    private void login() throws MalformedURLException, IOException, ParseException{
-         String url = "http://beyoutifulstudio.herokuapp.com/api/technicians?email="
-                 +userField.getText()+"&password="+passField.getText();
-       URL obj = new URL(url);
-       HttpURLConnection con = (HttpURLConnection)obj.openConnection();
-       con.setRequestProperty("Authorization", "Basic UDAwayFvb0ZmZjo=");
-       con.setRequestProperty("Content-Type", "application/json");
-       con.setRequestProperty("Accept", "application/json");
-       int responceCode= con.getResponseCode();
-        System.out.println("Responce code "+responceCode);
-        BufferedReader in = new BufferedReader( new InputStreamReader(con.getInputStream())); 
-        String inputLine; StringBuffer response = new StringBuffer();   
-        while ((inputLine = in.readLine()) != null) { 
-            response.append(inputLine); 
-        } 
-        in.close();   
-    //print result 
+
+    private void login() throws MalformedURLException, IOException, ParseException {
+        String url = "http://beyoutifulstudio.herokuapp.com/api/technicians?email="
+                + userField.getText() + "&password=" + passField.getText();
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestProperty("Authorization", "Basic UDAwayFvb0ZmZjo=");
+        con.setRequestProperty("Content-Type", "application/json");
+        con.setRequestProperty("Accept", "application/json");
+        int responseCode = con.getResponseCode();
+        System.out.println("Responce code " + responseCode);
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        if (responseCode == 200) {
+            //loginLabel.setText("Welcome "+ userField.getText());
+            //items.setDisable(false);
+            // client.setDisable(false);
+            System.out.println("yeah");
+        }
+        if (responseCode == 204) {
+            // loginLabel.setText("User/Password not found.");
+            System.out.println("bummer!");
+        }
+        in.close();
+        //print result 
         System.out.println(response.toString());
-        JSONParser parser=new JSONParser();
+        JSONParser parser = new JSONParser();
         Object object = parser.parse(response.toString());
-        System.out.println("object "+ object);
+        System.out.println("object " + object);
+
     }
 }
